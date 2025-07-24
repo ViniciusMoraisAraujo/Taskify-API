f58963a8b8a56424937e35c08c585459cd684d49
Lonezin <vinidev.morais@gmail.com>
2025-07-24 15:20:08 -0300

    feat(taskItem): add CreateTaskViewModel

---
+++ TaskifyAPI/ViewModels/TaskItem/CreateTaskItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TaskifyAPI.Enums;
+using TaskifyAPI.Models;
+
+namespace TaskifyAPI.ViewModels.TaskItem;
+
+public class CreateTaskItemViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string userName { get; set; }
+    public StatusTask StatusTask { get; set; }
+}
